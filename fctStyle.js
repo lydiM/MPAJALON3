df7b003e8cbe0fa1412a7582fb853d9a1941b88a
lydiM <33433387+lydiM@users.noreply.github.com>
2018-02-21 11:46:04 +0100

    Add files via upload
    commit après modification du code pour le changement de style des objets sur la carte

--- fctStyle.js
+++ fctStyle.js
@@ -10,13 +10,14 @@ var newstyle=function changestyle(){
 
     var contour = strokeSelect.value;
     var remplissage=fillSelect.value;
+
     var arcStyle;
     if (remplissage != "trans"){
     arcStyle = [
         new ol.style.Style({
             fill:new ol.style.Fill({color: remplissage, width: 1}),
-            stroke: new ol.style.Stroke({color: contour, width: 1})
-
+            stroke: new ol.style.Stroke({color: contour, width: 1}),
+            
         })
     ];}
     else {
--- scriptMap.js
+++ scriptMap.js
@@ -1,39 +1,62 @@
 
-
 var draw;
 var value= 'Circle';
-
   //**** l'objet arcs
   var arcs = new ol.Feature({
          type: (value),
          geometryFunction: geometryFunction
 
         });
-
+arcs.setStyle(newstyle);
 //**  la carte
 /*Déclaration d'une première couche raster pour l'affichage de la carte*/
 var raster = new ol.layer.Tile({
+
     source: new ol.source.OSM()
 });
 
 /*Déclaration d'une deuxième couche vector pour l'affichage du du dessin*/
-var source = new ol.source.Vector({
-            features:[arcs],
-           wrapX: false
-
-                                });
-
-/*Définition du style pour le dessin*/
-var vector = new ol.layer.Vector({
-  source: source,
-  style: newstyle
-    });
-
     var selectArc = new ol.interaction.Select({
-      condition: ol.events.condition.pointerMove
+      condition: ol.events.condition.pointerMove,
+      //style : newstyle,
+    });
+    var selectArc1 = new ol.interaction.Select({
+        style : newstyle,
+        condition: ol.events.condition.click,
+
     });
 
-    var select = null;
+
+    var  selectArc2= new ol.interaction.Select({
+      style : newstyle,
+      condition: ol.events.condition.click,
+
+    });
+
+
+    var source = new ol.source.Vector({
+              //  features:selectArc.getFeatures(),
+
+               features:[arcs],
+               wrapX: false
+
+             });
+
+    var vector1 = new ol.layer.Vector({
+    style: new ol.style.Style({
+          fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+          }),
+          stroke: new ol.style.Stroke({
+            color: 'Black',
+            width: 2
+          }),
+        }),
+
+      source: source
+      //style: (typeSelect.onchange =changestyle())
+        });
+
 
     var translate = new ol.interaction.Translate({
       features: selectArc.getFeatures()
@@ -41,18 +64,19 @@ var vector = new ol.layer.Vector({
 
 /*Ajout des deux couches au conteneur*/
 
+
 var map = new ol.Map({
 
-  interactions: ol.interaction.defaults().extend([selectArc, translate]),
+  //interactions: ol.interaction.defaults().extend([selectArc,selectArc1,selectArc2]),
 
-  layers: [raster, vector],
+  layers: [raster, vector1],
+
   target: 'map',
   view: new ol.View({
   center: ol.proj.transform([-4.49702, 48.39888], 'EPSG:4326', 'EPSG:3857'), //zoom sur l'UBO
   zoom:14
      })
    });
-
 
 /*Fonction qui permet de créer le dessin */
 //**
@@ -63,24 +87,42 @@ function addInteraction() {
     geometryFunction: geometryFunction
   });
 
-  map.addInteraction(draw);
-  map.removeInteraction(selectArc)
 
+  map.addInteraction(draw);
+//  map.removeInteraction(selectArc)
+map.addInteraction(selectArc2);
 
 
 }// fin de la fonction addInteraction
-function deplacer(vector){
+
+function deplacer(){
   map.addInteraction(selectArc);
+  map.addInteraction(translate);
 }
 
-function dessin(vector) {
+function selectionner1(){
+
+  map.addInteraction(selectArc1);
+
+}
+
+function selectionner2(){
+  map.addInteraction(selectArc2);
+
+}
+
+
+function dessin() {
  addInteraction(draw);
+ //addInteraction(draw2);
 
 }
 
 
-function supp(vector) {
+
+function supp() {
   map.removeInteraction(draw);
-  map.removeInteraction(selectArc)
+  map.removeInteraction(selectArc);
+
 }
 //************
